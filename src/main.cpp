c33cf802706d89d84bf647ee5f0ed28ec5833080
Hopkins <>
2024-09-29 03:49:06 +0800

    Create basic snake class and its data structures

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,92 @@
+#include <cstdint>
 #include <cstdio>
 #include <termios.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <vector>
+
+using namespace std;
+
+class Snake {
+	private:
+		int fieldWidth, fieldHeight;
+		int headpos, tailpos;
+		int food;		// Food eaten
+
+		// 0 - Up
+		// 1 - Down
+		// 2 - Left
+		// 3 - Right
+		uint8_t direction;		// Head direction
+		vector<uint8_t> body;	// Does not keep track of head
+
+		void grow() {
+			body.insert(body.begin(), direction);
+			switch (direction) {
+				case 0:
+					headpos -= fieldHeight;
+					break;
+				case 1:
+					headpos += fieldHeight;
+					break;
+				case 2:
+					headpos--;
+					break;
+				case 3:
+					headpos++;
+					break;
+			}
+		}
+
+	public:
+		// Init snake
+		Snake(int fieldWidth, int fieldHeight, int startPos, int startLength) {
+			this -> fieldWidth = fieldWidth;
+			this -> fieldHeight = fieldHeight;
+
+			direction = 3;
+			headpos = startPos;
+			tailpos = startPos;
+			if (startLength < 1) {
+				startLength = 1;
+			}
+		}
+		
+		// Getters for head and tail positions
+		int getHeadPos() {
+			return headpos;
+		}
+		int getTailPos() {
+			return tailpos;
+		}
+
+		// Updates snake position; returns false if snake grew
+		bool move() {
+			grow();
+			// Consumes food to grow
+			if (food > 0) {
+				food--;
+				return false;
+			}
+			// No food to grow; remove tail
+			switch (body.back()) {
+				case 0:
+					tailpos -= fieldHeight;
+					break;
+				case 1:
+					tailpos += fieldHeight;
+					break;
+				case 2:
+					tailpos--;
+					break;
+				case 3:
+					tailpos++;
+					break;
+			}
+			body.pop_back();
+			return true;
+		}
+};
 
 // Initialize interface
 void ginit() {
@@ -41,6 +126,9 @@ void inputBlocking(bool enable) {
 		fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
 }
 
+void drawColoredString(char* color, char* text) {
+	// printf();
+}
 
 int main (int argc, char *argv[]) {
 	ginit();
@@ -48,8 +136,14 @@ int main (int argc, char *argv[]) {
 	gotoxy(100, 16);
 	printf("\e[48;5;82m  ");
 	printf("\e[0m");
+	printf("  ");
+	printf("\e[42m  ");
+	printf("\e[0m");
 	gotoxy(100, 18);
 	printf("Hello World");
+
+
+	int field[20];
 
 	inputBlocking(false);
 	int color = 0, kp = 0;
